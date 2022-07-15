9a85e40ff0d4d237fc85a3bba40c13da68fb60a0
Mike Ross <a1c.m.ross@gmail.com>
2022-07-15 08:33:54 -0500

    Makes url dynamic

--- f-ampeg/src/test/java/Framework/SeleniumTestBase.java
+++ f-ampeg/src/test/java/Framework/SeleniumTestBase.java
@@ -8,18 +8,30 @@ import org.testng.annotations.BeforeTest;
 
 public abstract class SeleniumTestBase {
 	protected WebDriver driver;
-	protected final String BASE_URL = "http://www.ampeg.com";
+	protected String baseUrl;
+
+	public SeleniumTestBase(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
 
 	@BeforeTest
 	protected void setup() {
 		System.setProperty("webdriver.chrome.driver", "c:\\temp\\chromedriver.exe");
 		this.driver = new ChromeDriver();
-		//this.driver.manage().window().maximize();
 		this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
 	}
 
 	@AfterTest
 	protected void cleanup() {
 		this.driver.quit();
+	}
+	
+	protected WebDriver getDriver() {
+		return this.driver;
+	}
+	
+	protected String getBaseUrl() {
+		return baseUrl;
 	}
 }
