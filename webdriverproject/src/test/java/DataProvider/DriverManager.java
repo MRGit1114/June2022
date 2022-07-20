b2a6f1ea02ad85c3b3f800dc0b32cb27e77b6d63
Mike Ross <a1c.m.ross@gmail.com>
2022-07-20 06:25:07 -0500

    Adds createDriver(), getDriver(), setDriver(), quitDriver() according to UML diagram

--- webdriverproject/src/test/java/DataProvider/DriverManager.java
+++ webdriverproject/src/test/java/DataProvider/DriverManager.java
@@ -1,17 +1,50 @@
 package DataProvider;
 
+import org.openqa.selenium.WebDriver;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Driver;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
-	
+
+	protected ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+	private Driver WebDriver;
 	private Properties properties;
 	protected final String propertyFilePath= "src//main//resources//config.properties";
 
-	
+	public WebDriver createDriver() {
+		return null;
+	}
+
+	public WebDriver getDriver() {
+		if (null == drivers.get()) {
+			drivers.set(this.createDriver());
+		}
+		drivers.get().manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+
+		return drivers.get();
+	}
+
+	protected void setDriver(WebDriver driver) {
+		return;
+	}
+
+	public void quitDriver() {
+		if (null != drivers.get()) {
+			try {
+				drivers.get().quit();
+				drivers.remove();
+			} catch (Exception e) {
+				System.err.println("Unable to gracefully quit WebDriver." +e);
+			}
+
+		}
+	}
+
 	public DriverManager(){
 		BufferedReader reader;
 		try {
@@ -28,29 +61,28 @@ public class DriverManager {
 			throw new RuntimeException("config.properties not found at " + propertyFilePath);
 		}		
 	}
-	
+
 	public String getDriverPath(){
 		String driverPath = properties.getProperty("driverPath");
 		if(driverPath!= null) return driverPath;
 		else throw new RuntimeException("driverPath not specified in the config.properties file.");		
 	}
-	
+
 	public long getImplicitlyWait() {		
 		String implicitlyWait = properties.getProperty("implicitlyWait");
 		if(implicitlyWait != null) return Long.parseLong(implicitlyWait);
 		else throw new RuntimeException("implicitlyWait not specified in the config.properties file.");		
 	}
-	
+
 	public String getApplicationUrl() {
 		String url = properties.getProperty("url");
 		if(url != null) return url;
 		else throw new RuntimeException("url not specified in the config.properties file.");
 	}
-	
+
 	public String getBrowser() {
 		String browser = properties.getProperty("browser");
 		if(browser != null) return browser;
 		else throw new RuntimeException("browser not specified in the config.properties file.");
 	}
-
 }
