32bf7e6342a9ee1f598414cb8c7726ceff229d8b
Mike Ross <a1c.m.ross@gmail.com>
2022-07-20 06:23:30 -0500

    Adds ChromeDriverManager

--- webdriverproject/src/test/java/DataProvider/ChromeDriverManager.java
+++ webdriverproject/src/test/java/DataProvider/ChromeDriverManager.java
@@ -1,6 +1,26 @@
 package DataProvider;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import DataProvider.ChromeDriverManager;
+
 public class ChromeDriverManager extends DriverManager {
-	void createDriver() {
+	
+	@Override
+	public WebDriver createDriver() {
+		System.out.println("Initializing Chrome Driver");
+		//WebDriverManager.getInstance(CHROME).setup();
+		return new ChromeDriver(getChromeOptions());
+	}
+
+	private ChromeOptions getChromeOptions() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		options.addArguments("--start-maximized");
+		options.addArguments("--disable-features=EnableEphemeralFlashPermission");
+		options.addArguments("--disable-infobars");
+
+		return options;
 	}
 }
