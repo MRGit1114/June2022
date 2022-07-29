77ec26145ceb752d076b3f1e553f0d49fb97878d
Mike Ross <a1c.m.ross@gmail.com>
2022-07-28 19:30:03 -0500

    Adds JSONListResponse

---
+++ api.project/src/test/java/getRequest/JSONListResponse.java
@@ -0,0 +1,12 @@
+package getRequest;
+import java.util.List;
+
+public class JSONListResponse {
+
+	public String page;
+	public String per_page;
+	public String total;
+	public String total_pages;
+	public List<JSONData> data;
+	public JSONSupport support;
+}
