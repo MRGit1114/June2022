b3cc5f158182f2c654280be07d4f42c304fed2c0
Mike Ross <a1c.m.ross@gmail.com>
2022-08-08 17:57:16 -0500

    Adds Steps step definition file

---
+++ f-ampeg/src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,14 @@
+package stepDefinitions;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class Steps {
+	//after JUnit runs TestRunner successfully, add my code for each step here
+}
