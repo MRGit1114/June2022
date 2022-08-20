34386df094a118f6188875b5623868cd2a59c406
Mike Ross <a1c.m.ross@gmail.com>
2022-08-19 20:47:20 -0500

    Updates Steps

--- f-ampeg/src/test/java/stepDefinitions/Steps.java
+++ f-ampeg/src/test/java/stepDefinitions/Steps.java
@@ -1,16 +1,21 @@
 package stepDefinitions;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
+import cucumber.api.CucumberOptions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import java.util.concurrent.TimeUnit;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue={"stepDefinition"}
+		)
 
 public class Steps {
 	private WebDriver driver = null;
