316f35897efc5b11479c0899be51309602a2c065
Mike Ross <a1c.m.ross@gmail.com>
2022-08-19 20:47:55 -0500

    Updates TestRunner, moves cucumber items to Steps

--- f-ampeg/src/test/java/TestRunner/TestRunner.java
+++ f-ampeg/src/test/java/TestRunner/TestRunner.java
@@ -1,15 +1,5 @@
 package TestRunner;
 
-import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "Feature"
-		,glue={"stepDefinition"}
-		)
-
 public class TestRunner {
 
 }
